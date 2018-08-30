aaa0d033cccd120af26af3391122f7d0bf8d554e
Konstantinos Kritharidis <konstantinosk31@gmail.com>
2018-08-30 14:01:49 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Space_Invaders_2
+{
+    class Player
+    {
+        public bool alive;
+        public char icon = 'W';
+        public void Move() { }
+        public void Shoot() { }
+    }
+}
